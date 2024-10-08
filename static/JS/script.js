629563183274233ff91173cdc33bd95d9e0baf40
Gigichx <lattanzio.luigi@panettipitagora.edu.it>
2024-10-08 09:51:27 +0200

    indovina-numero

--- static/JS/script.js
+++ static/JS/script.js
@@ -33,37 +33,42 @@ function sanitize(Var) {
     return sanificata
 }
 
+function handleClick(event) {
+    let input = document.querySelector("#guess").value;
+    numeroInserito = sanitize(input);
+
+    if (isNaN(numeroInserito)) {
+        console.log("Inserisci un numero valido")
+    } else {
+        if (numeroInserito > numEFFETIVO) {
+            console.log("Il numero '" + numeroInserito + "' è troppo grande")
+            tentativi -= 1
+            contatore += 1
+        } else if (numeroInserito < numEFFETIVO) {
+            console.log("Il numero '" + numeroInserito + "' è troppo piccolo")
+            tentativi -= 1
+            contatore += 1
+        } else if (numeroInserito === numEFFETIVO) {
+            console.log("Bravo, hai vinto!")
+            indovinato = true
+            document.querySelector("#guess").disabled = true;
+            document.querySelector("#guessBtn").disabled = true;
+        }
+    }
+    if (tentativi === 0 && !indovinato) {
+        console.log("Hai esaurito i tentativi, hai perso!")
+        document.querySelector("#guess").disabled = true;
+        document.querySelector("#guessBtn").disabled = true;
+    }
+}
+
+
+console.log("Benvenuto a 'INDOVINA IL NUMERO!'")
 let tentativi = 5
 let contatore = 0
 let indovinato = false
 let numEFFETIVO = generateRandomInteger()
 let numeroInserito
-console.log("Benvenuto a 'INDOVINA IL NUMERO!'")
-while (contatore < 5 && indovinato === false) {
-    console.log("Hai ancora " + tentativi + " tentativi")
-    do {
-        let input = prompt("Inserisci un numero")
-        numeroInserito = sanitize(input)
-        if (isNaN(numeroInserito)) {
-            console.log("Inserisci un numero valido")
-        } else {
-            break;
-        }
-    } while (true)
-
-    if (numeroInserito > numEFFETIVO) {
-        console.log("Il numero '" + numeroInserito + "' è troppo grande")
-        tentativi -= 1
-        contatore += 1
-    } else if (numeroInserito < numEFFETIVO) {
-        console.log("Il numero '" + numeroInserito + "' è troppo piccolo")
-        tentativi -= 1
-        contatore += 1
-    } else if (numeroInserito === numEFFETIVO) {
-        console.log("Bravo, hai vinto!")
-        indovinato = true
-    }
-}
-if (!indovinato) {
-    console.log("Mi dispiace, hai perso. Il numero era " + numEFFETIVO);
-}
+let userField = document.querySelector("#guess")
+let btn = document.querySelector("#guessBtn")
+btn.addEventListener("click", handleClick)
