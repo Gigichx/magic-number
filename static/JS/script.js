6e63b44d0cd20a046b0cb7a980f13e3cf49a0d6f
Gigichx <lattanzio.luigi@panettipitagora.edu.it>
2024-10-15 11:45:41 +0200

    sistemare l'elenco

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,20 +1,3 @@
-//Qui scriviamo il codice JS
-/*
-Scrivere una funzione che generi un numero casuale intero compreso fra un minimo e un max
-
-function generateRandomInteger(min, max){
-    // qui devo usare Math.random
-}
-
-Usare questa funzione per sviluppare il gioco "indovina il numero":
-generare un numero casuale compreso fra 1 e 100 
-chiedere in input all'utente un numero finchè non esaurisce 5 tentativi
-fare sanitize dell'input
-se il numero inserito dall'utente è maggiore del numero generato stampa in console la stringa "Troppo grande"
-se il numero inserito dall'utente è minore del numero generato stampa in console la stringa "Troppo piccolo"
-se il numero inserito dall'utente è uguale del numero generato stampa in console la stringa "Bravo, hai vinto"
-Il gioco finisce se l'utente esaurisce i 5 tentativi o se indovina
-*/
 
 function generateRandomInteger(min = 1, max = 100) {
     const minCeiled = Math.ceil(min);
